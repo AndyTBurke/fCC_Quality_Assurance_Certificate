121901786ef60a7c554b44e64fdaf2cf5ddcc000
Andrew Burke <107972213+AndyTBurke@users.noreply.github.com>
2023-01-10 22:09:22 -0600

    Create american-to-british-titles.js
    trasnferred from replit

---
+++ Project_5/Files/american-to-british-titles.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'mr.': 'mr',
+  'mrs.': 'mrs',
+  'ms.': 'ms',
+  'mx.': 'mx',
+  'dr.': 'dr',
+  'prof.': 'prof',
+  'Mrs.': 'Mrs',
+  'Mr.': 'Mr',
+  'Ms.': 'Ms',
+  'Mx.': 'Mx',
+  'Dr.': 'Dr',
+  'Prof.': 'Prof'
+}
